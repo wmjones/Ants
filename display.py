8b016d427b8bd40ee610e92e4595d2a5fadd88c7
Wyatt <wyattjo116@gmail.com>
2015-01-28 20:57:15 -0500

    fixed the neighborhood problem

--- display.py
+++ display.py
@@ -22,8 +22,8 @@ def graphHexGrid(x_length, y_length, edge_length, matrix):
   image = Image.new('RGB', (x_length, y_length), 'white')
   draw = ImageDraw.Draw(image)
   hexagon_generator = HexagonGenerator(edge_length)
-  for row in [4]:#range(len(matrix)):
-      for col in [6]:#range(len(matrix)):
+  for row in range(len(matrix)):
+      for col in range(len(matrix)):
           hexagon = hexagon_generator(row, col)
           if matrix[row][col]==1:
               draw.polygon(list(hexagon), fill='black')
@@ -35,6 +35,6 @@ n=2*4
 myWorld=[]
 myWorld=[[0 for j in range(n)] for i in range(n)]
 
-graphHexGrid(500,500,10,myWorld)
+#graphHexGrid(500,500,10,myWorld)
 #perhaps set up so that I call it from main.py
 #[i-1,j],[i-1,j+1],[i,j+1],[i+1,j],[i+1,j-1],[i,j-1]
--- main.py
+++ main.py
@@ -16,25 +16,25 @@ population=[]
 for i in range(numOfAnts):
     population.append(Ant(i))
 
-#for t in range(5):
-#    graphHexGrid(500,500,10,myWorld)
-#    for i in range(len(population)):
-#        a, b = (population[i].p)[0], (population[i].p)[1]
-#        myWorld[a][b]=1
-#        Ant.look(population[i],*neighborhood(myWorld,*(population[i].p)))
-myWorld[5][5]=1
-graphHexGrid(500,500,10,myWorld)
-myWorld[4][5]=1
-graphHexGrid(500,500,10,myWorld)
-myWorld[5][6]=1
-graphHexGrid(500,500,10,myWorld)
-myWorld[6][6]=1
-graphHexGrid(500,500,10,myWorld)
-myWorld[6][5]=1
-graphHexGrid(500,500,10,myWorld)
-myWorld[5][4]=1
-graphHexGrid(500,500,10,myWorld)
-myWorld[4][4]=1
-graphHexGrid(500,500,10,myWorld)
+for t in range(5):
+    graphHexGrid(500,500,10,myWorld)
+    for i in range(len(population)):
+        a, b = (population[i].p)[0], (population[i].p)[1]
+        myWorld[a][b]=1
+        Ant.look(population[i],*neighborhood(myWorld,*(population[i].p)))
+#myWorld[5][5]=1
+#graphHexGrid(500,500,10,myWorld)
+#myWorld[4][5]=1
+#graphHexGrid(500,500,10,myWorld)
+#myWorld[5][6]=1
+#graphHexGrid(500,500,10,myWorld)
+#myWorld[6][6]=1
+#graphHexGrid(500,500,10,myWorld)
+#myWorld[6][5]=1
+#graphHexGrid(500,500,10,myWorld)
+#myWorld[5][4]=1
+#graphHexGrid(500,500,10,myWorld)
+#myWorld[4][4]=1
+#graphHexGrid(500,500,10,myWorld)
 
 print myWorld
